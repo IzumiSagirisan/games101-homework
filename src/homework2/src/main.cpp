12b4f3d5e9e68b3038b2e8d763be6a9cb0321e16
IzumiSagirisann <arcstars@163.com>
2026-02-06 11:27:18 +0800

    feat:完成homework2基础模块

---
+++ src/homework2/src/main.cpp
@@ -0,0 +1,167 @@
+// clang-format off
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "rasterizer.hpp"
+#include "global.hpp"
+#include "Triangle.hpp"
+
+constexpr double MY_PI = 3.1415926;
+
+Matrix4f get_view_matrix(Vector3f eye_pos)
+{
+    Matrix4f view = Matrix4f::Identity();
+
+    Matrix4f translate;
+    translate << 1,0,0,-eye_pos[0],
+                 0,1,0,-eye_pos[1],
+                 0,0,1,-eye_pos[2],
+                 0,0,0,1;
+
+    view = translate*view;
+
+    return view;
+}
+
+Matrix4f get_model_matrix(float rotation_angle)
+{
+    Matrix4f model = Matrix4f::Identity();
+    return model;
+}
+
+Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
+{
+    // Copy-paste your implementation from the previous assignment.
+    Matrix4f projection = Matrix4f::Identity();
+    // Create the projection matrix for the given parameters.
+    // Then return it.
+
+    // 计算lr,nf,tb的值，随后根据分解矩阵求解
+    // 需要注意，t必须是正数，因为t表示近平面上边界在y方向的坐标（高度），这是一个长度，必须是正数
+    // 同样，n表示近平面离相机的距离。因为GAMES101中认为相机是看向-z轴，为了表示数轴上的真实位置，可以进行取反处理
+    float l, r, n, f, t, b;
+    // 这里可以取反，表示坐标值
+    n = zNear;
+    f = zFar;
+
+    // 注意，n是在数轴上更大的值，因此应该对应t，因为t必须要是正的，因此这里取-n
+    // t,b 表示视锥体的上下边界，应该使用距离（正数）计算
+    t = -n * tan(eye_fov * MY_PI / 180 / 2);
+    b = -t;
+
+    r = aspect_ratio * t;
+    l = aspect_ratio * b;
+
+    // 构造透视投影转正交投影矩阵
+    Matrix4f perspToOrtho = Matrix4f::Zero();
+    perspToOrtho(0, 0) = n;
+    perspToOrtho(1, 1) = n;
+    perspToOrtho(2, 2) = n + f;
+    perspToOrtho(2, 3) = - n * f;
+    perspToOrtho(3, 2) = 1;
+
+    Matrix4f orthoTranslate = Matrix4f::Identity();
+    orthoTranslate(0, 3) = -(l + r) / 2;
+    orthoTranslate(1, 3) = -(t + b) / 2;
+    orthoTranslate(2, 3) = -(f + n) / 2;
+
+    Matrix4f orthoScale = Matrix4f::Identity();
+    orthoScale(0, 0) = 2 / (r - l);
+    orthoScale(1, 1) = 2 / (t - b);
+    orthoScale(2, 2) = 2 / (n - f);
+
+    // 矩阵合成
+    projection = orthoScale * orthoTranslate * perspToOrtho;
+
+    return projection;
+}
+
+int main(int argc, const char** argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc == 2)
+    {
+        command_line = true;
+        filename = std::string(argv[1]);
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Vector3f eye_pos = {0,0,5};
+
+
+    std::vector<Vector3f> pos
+            {
+                    {2, 0, -2},
+                    {0, 2, -2},
+                    {-2, 0, -2},
+                    {3.5, -1, -5},
+                    {2.5, 1.5, -5},
+                    {-1, 0.5, -5}
+            };
+
+    std::vector<Vector3i> ind
+            {
+                    {0, 1, 2},
+                    {3, 4, 5}
+            };
+
+    std::vector<Vector3f> cols
+            {
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {217.0, 238.0, 185.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0},
+                    {185.0, 217.0, 238.0}
+            };
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+    auto col_id = r.load_colors(cols);
+
+    int key = 0;
+    int frame_count = 0;
+
+    if (command_line)
+    {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while(key != 27)
+    {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, col_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+    }
+
+    return 0;
+}
+// clang-format on
